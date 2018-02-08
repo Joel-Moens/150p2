954a73c91a36b38e7c72e4dea4e32b37d73b94f0
John Tracy <jwtracy@ucdavis.edu>
2018-02-08 13:04:48 -0800

    defines

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -11,7 +11,19 @@
 #include "queue.h"
 #include "uthread.h"
 
+#define READY 0
+#define RUNNING 1
+#define BLOCKED 2
+#define ZOMBIE 3
+
 /* TODO Phase 2 */
+int tidCount = 0;
+typedef struct thread_block {
+	uthread_t id;
+	uthrad_ctx_t *context;
+	void * sp;
+	int state;
+}thread_block;
 
 void uthread_yield(void)
 {
@@ -26,6 +38,16 @@ uthread_t uthread_self(void)
 int uthread_create(uthread_func_t func, void *arg)
 {
 	/* TODO Phase 2 */
+	
+	thread_block * tblock;
+	tblock = malloc(sizeof(tblock));
+	tblock-> id = tidCount;
+	tblock->sp = uthread_ctx_alloc_stack();
+	tblock->context = malloc(sizeof(uthread_ctx_t));
+	//Make a thread block with a context pointer
+
+	//Call uthread_ctx_init with the new tblock and given arguments
+	uthread_ctx_init(tblock->context, tblock->sp, func, arg);
 }
 
 void uthread_exit(int retval)
