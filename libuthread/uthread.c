a120f60092506f26e9eb5dbc9c76da5665b2ba29
Joel Moens <jmmoens@ucdavis.edu>
2018-02-08 13:08:45 -0800

    My changes

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -12,6 +12,15 @@
 #include "uthread.h"
 
 /* TODO Phase 2 */
+int tidCount = 0;
+typedef struct thread_block {
+	uthread_t id;
+	uthrad_ctx_t *context;
+	void * sp;
+	int state;
+}thread_block;
+
+queue_t tQueue;
 
 void uthread_yield(void)
 {
@@ -26,6 +35,20 @@ uthread_t uthread_self(void)
 int uthread_create(uthread_func_t func, void *arg)
 {
 	/* TODO Phase 2 */
+	if (tidCount == 0)
+	{
+
+
+	}
+	thread_block * tblock;
+	tblock = malloc(sizeof(tblock));
+	tblock-> id = tidCount;
+	tblock->sp = uthread_ctx_alloc_stack();
+	tblock->context = malloc(sizeof(uthread_ctx_t));
+	//Make a thread block with a context pointer
+
+	//Call uthread_ctx_init with the new tblock and given arguments
+	uthread_ctx_init(tblock->context, tblock->sp, func, arg);
 }
 
 void uthread_exit(int retval)
@@ -37,5 +60,13 @@ int uthread_join(uthread_t tid, int *retval)
 {
 	/* TODO Phase 2 */
 	/* TODO Phase 3 */
+}
+
+void init(void)
+{
+	//Allocate the queue
+	tQueue = queue_create();
+	//Init the main thread
+	thread_block * tmain;
 }
 
